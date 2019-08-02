6373094052352a50d9a13e708f418808b2c9a22d
Jacob Haas <jacob_haas_7@hotmail.com>
2019-08-01 20:50:06 -0700

    inital commit, mobile portion

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './main.css';
+
+function App() {
+  return (
+    <div className="App">
+
+<div id="bg">
+        <header>
+          <a href="#">Zoom</a>
+        </header>
+
+        <main>
+
+          <section id="card">
+            <ul>
+              <li>
+                <span></span>
+                <strong>How may I help you?</strong>
+              </li>
+              <li>
+                <span></span>
+                <strong>How may I help you?</strong>
+              </li>
+              <li>
+                <span></span>
+                <strong>How may I help you?</strong>
+              </li>
+            </ul>
+          </section>
+
+          <section id="primary">
+            <h1>Your Personal Assistant</h1>
+            <p>Get help with your daily stuff</p>
+
+            <a href="#">Get help</a>
+          </section>
+
+        </main>
+
+
+        </div>
+    </div>
+  );
+}
+
+export default App;
